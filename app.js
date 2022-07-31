68468ec1449f056f15a53f1e9380ff1728d8555c
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-07-31 19:25:12 -0300

    Server On , MVC ok

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const routerMain = require('./routers/mainRouter.js');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use('/',routerMain );
+
+app.listen(3001, ()=>console.log('Server running...'+"http://localhost:3001"))
