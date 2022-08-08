f1071e2ab19c433a19bf48985897957a3aeff6dc
Estación Aérea Mar del Plata EAMP <jorgemcarlotto@gmail.com>
2022-08-08 19:27:51 -0300

    horas a decimal

--- controller/mainController.js
+++ controller/mainController.js
@@ -7,7 +7,8 @@ index: function(req,res){
 },
 
 cargarDatos: function(req,res){
-    // console.log(req.body)
+     
+    //console.log(req.body)
 
     let latGrados = req.body.latGrados;
     let latMinutos = req.body.latMinutos;
@@ -16,17 +17,50 @@ cargarDatos: function(req,res){
     let longGrados = req.body.longGrados;
     let longMinutos = req.body.longMinutos;
     let longSegundos = req.body.longSegundos;
+    
+
+    let rumbo = req.body.rumbo;
    
 let latitudDecimal = geolib.toDecimal(`${latGrados}° ${latMinutos}' ${latSegundos}" S`); 
 
 let longitudDecimal = geolib.toDecimal(`${longGrados}° ${longMinutos}' ${longSegundos}" W`); 
-    
+   
 
-console.log(latitudDecimal)
-console.log(longitudDecimal)
-    
+let puntoDesplazado = geolib.computeDestinationPoint(
+    [longitudDecimal, latitudDecimal],
+    674128,
+    rumbo
+);
+//console.log(latitudDecimal)
+//console.log(longitudDecimal)
+ 
+//console.log(geolib.decimalToSexagesimal(puntoDesplazado.latitude))
+//console.log(geolib.decimalToSexagesimal(puntoDesplazado.longitude))
+
+
+
+
+//Pasa a horas a decimales 
+function timeToDecimal(t) {
+    var arr = t.split(':');
+    var dec = parseInt((arr[1]/6)*10, 10);
+
+    return parseFloat(parseInt(arr[0], 10) + '.' + (dec<10?'0':'') + dec);
+} 
+
+console.log(timeToDecimal('01:30'))
+
+
+
+
+
+
+
+
 },
    
+
+
 }
 
 module.exports = mainController;
