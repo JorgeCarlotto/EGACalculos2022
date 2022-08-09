93f4e9fb66e6973a1f080b703cb89fca98e0fefe
Estación Aérea Mar del Plata EAMP <jorgemcarlotto@gmail.com>
2022-08-09 12:42:25 -0300

    funcion punto desplÃazado ok

--- controller/mainController.js
+++ controller/mainController.js
@@ -10,7 +10,7 @@ index: function(req,res){
 
 cargarDatos: function(req,res){
      
-    //console.log(req.body)
+    console.log(req.body)
 
     let latGrados = req.body.latGrados;
     let latMinutos = req.body.latMinutos;
@@ -27,52 +27,23 @@ let latitudDecimal = geolib.toDecimal(`${latGrados}° ${latMinutos}' ${latSegund
 
 let longitudDecimal = geolib.toDecimal(`${longGrados}° ${longMinutos}' ${longSegundos}" W`); 
    
+let firstTime = moment(req.body.firstTime);//now
+let secondTime  = moment(req.body.secondTime);
+let horaDecimales = ((secondTime.diff(firstTime, 'minutes'))/60).toFixed(1)
+let distanciaEnMetros = ((req.body.velocidad) * horaDecimales) * 1852 ;
 
 let puntoDesplazado = geolib.computeDestinationPoint(
     [longitudDecimal, latitudDecimal],
-    674128,
+    distanciaEnMetros,
     rumbo
 );
-//console.log(latitudDecimal)
-//console.log(longitudDecimal)
+// console.log(latitudDecimal)
+// console.log(longitudDecimal)
  
-//console.log(geolib.decimalToSexagesimal(puntoDesplazado.latitude))
-//console.log(geolib.decimalToSexagesimal(puntoDesplazado.longitude))
+ console.log(geolib.decimalToSexagesimal(puntoDesplazado.latitude))
+ console.log(geolib.decimalToSexagesimal(puntoDesplazado.longitude))
 
-
-
-
-//Pasa a horas a decimales 
-function timeToDecimal(t) {
-    var arr = t.split(':');
-    var dec = parseInt((arr[1]/6)*10, 10);
-
-    return parseFloat(parseInt(arr[0], 10) + '.' + (dec<10?'0':'') + dec);
-} 
-
-console.log(timeToDecimal('01:30'))
-
-
-
-// var oldDate = new Date("2022-07-09T08:48:34.000Z");
-// var today = new Date();
-
-// console.log(DifferenceInDays(oldDate, today));
-
-// function DifferenceInDays(firstDate, secondDate) {
-//     return Math.round((secondDate-firstDate)/(1000*60*60*24));
-// } 
-
-
-var b = moment('2022-08-09T10:29:00');//now
-var a = moment('2022-08-10T10:45:00');
-
-console.log(a.diff(b, 'minutes')) 
-console.log(a.diff(b, 'hours')) 
-console.log(a.diff(b, 'days')) 
-console.log(a.diff(b, 'weeks')) 
-console.log(((a.diff(b, 'minutes'))/60).toFixed(1)) 
-
+           
 
 
 },
---
+++ controller/test.js
@@ -0,0 +1,21 @@
+//Pasa a horas a decimales 
+// function timeToDecimal(t) {
+//     var arr = t.split(':');
+//     var dec = parseInt((arr[1]/6)*10, 10);
+
+//     return parseFloat(parseInt(arr[0], 10) + '.' + (dec<10?'0':'') + dec);
+// } 
+
+// console.log(timeToDecimal('01:30'))
+
+
+// Moment
+// var b = moment('2022-08-09T10:29:00');//now
+// var a = moment('2022-08-10T10:45:00');
+
+// console.log(a.diff(b, 'minutes')) 
+// console.log(a.diff(b, 'hours')) 
+// console.log(a.diff(b, 'days')) 
+// console.log(a.diff(b, 'weeks')) 
+// console.log(((a.diff(b, 'minutes'))/60).toFixed(1)) 
+
