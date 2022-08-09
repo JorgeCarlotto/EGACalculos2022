9f75344bd2f12800068d1e4612decbdc676926b3
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-08-09 19:16:20 -0300

    distancia a la posicion inicial Ok

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,16 +1,13 @@
-const geolib = require('geolib');
-const moment =require('moment');
-
+const geolib = require("geolib");
+const moment = require("moment");
 
 let mainController = {
+  index: function (req, res) {
+    res.render("main");
+  },
 
-index: function(req,res){
-    res.render("main")
-},
-
-cargarDatos: function(req,res){
-     
-    console.log(req.body)
+  cargarDatos: function (req, res) {
+    //console.log(req.body);
 
     let latGrados = req.body.latGrados;
     let latMinutos = req.body.latMinutos;
@@ -19,37 +16,68 @@ cargarDatos: function(req,res){
     let longGrados = req.body.longGrados;
     let longMinutos = req.body.longMinutos;
     let longSegundos = req.body.longSegundos;
-    
 
     let rumbo = req.body.rumbo;
-   
-let latitudDecimal = geolib.toDecimal(`${latGrados}° ${latMinutos}' ${latSegundos}" S`); 
 
-let longitudDecimal = geolib.toDecimal(`${longGrados}° ${longMinutos}' ${longSegundos}" W`); 
-   
-let firstTime = moment(req.body.firstTime);//now
-let secondTime  = moment(req.body.secondTime);
-let horaDecimales = ((secondTime.diff(firstTime, 'minutes'))/60).toFixed(1)
-let distanciaEnMetros = ((req.body.velocidad) * horaDecimales) * 1852 ;
+    let latitudDecimal = geolib.toDecimal(
+      `${latGrados}° ${latMinutos}' ${latSegundos}" S`
+    );
 
-let puntoDesplazado = geolib.computeDestinationPoint(
-    [longitudDecimal, latitudDecimal],
-    distanciaEnMetros,
-    rumbo
-);
-// console.log(latitudDecimal)
-// console.log(longitudDecimal)
- 
- console.log(geolib.decimalToSexagesimal(puntoDesplazado.latitude))
- console.log(geolib.decimalToSexagesimal(puntoDesplazado.longitude))
+    let longitudDecimal = geolib.toDecimal(
+      `${longGrados}° ${longMinutos}' ${longSegundos}" W`
+    );
 
-           
+    let firstTime = moment(req.body.firstTime); //now
+    let secondTime = moment(req.body.secondTime);
+    let horaDecimales = (secondTime.diff(firstTime, "minutes") / 60).toFixed(1);
+    let distanciaEnMetros = req.body.velocidad * horaDecimales * 1852;
+
+    let puntoDesplazado = geolib.computeDestinationPoint(
+      [longitudDecimal, latitudDecimal],
+      distanciaEnMetros,
+      rumbo
+    );
+    // console.log(latitudDecimal)
+    // console.log(longitudDecimal)
+
+    let puntoDesplazadoFinal = {
+      latitude: geolib.decimalToSexagesimal(puntoDesplazado.latitude),
+      longitude: geolib.decimalToSexagesimal(puntoDesplazado.longitude),
+    };
+    //  console.log(geolib.decimalToSexagesimal(puntoDesplazado.latitude))
+    //  console.log(geolib.decimalToSexagesimal(puntoDesplazado.longitude))
+
+    
+
+    let sazm = {
+      latitude: -37.9297222,
+      longitude: -57.5763888,
+    };
+
+    let sazb = {
+      latitude: -38.72,
+      longitude: -62.1583333,
+    };
+
+    let savv = {
+      latitude: -40.8672222,
+      longitude: -63.0008333,
+    };
 
 
-},
-   
 
+    let distanciaSazm = ((geolib.getPreciseDistance(sazm,{ latitude: latitudDecimal, longitude: longitudDecimal }))/1852).toFixed(1);
 
-}
+    let distanciaSazb = ((geolib.getPreciseDistance(sazb,{ latitude: latitudDecimal, longitude: longitudDecimal }))/1852).toFixed(1);
+
+    let distanciaSavv = ((geolib.getPreciseDistance(savv,{ latitude: latitudDecimal, longitude: longitudDecimal }))/1852).toFixed(1);
+
+    
+    let distanciaAlBarcoDesdeAeropuertos = [distanciaSazm,distanciaSazb,distanciaSavv]
+
+    res.render("main", { posicionDesplazada: puntoDesplazadoFinal,distanciaInicial:distanciaAlBarcoDesdeAeropuertos });
+
+  },
+};
 
 module.exports = mainController;
--- controller/test.js
+++ controller/test.js
@@ -19,3 +19,25 @@
 // console.log(a.diff(b, 'weeks')) 
 // console.log(((a.diff(b, 'minutes'))/60).toFixed(1)) 
 
+let distanciaSazm = geolib.getPreciseDistance(  //da en mtrs
+{ latitude: -37.9297222, longitude: -57.5763888 }, //MDP
+{ latitude: -40.0806394, longitude: -58.1181208 }
+);
+
+let distanciaSazb = geolib.getPreciseDistance(  //da en mtrs
+{ latitude: -38.72, longitude: -62.1583333 }, //Bahia
+{ latitude: -40.0806394, longitude: -58.1181208 }
+);
+
+let distanciaSavv = geolib.getPreciseDistance(  //da en mtrs
+{ latitude: -40.8672222, longitude: -63.0008333 }, //Viedma
+{ latitude: -40.0806394, longitude: -58.1181208 }
+);
+
+let distanciaEntrePuntosFinalUno = (distanciaSazm/1852).toFixed(1)
+let distanciaEntrePuntosFinalDos = (distanciaSazb/1852).toFixed(1)
+let distanciaEntrePuntosFinalTres = (distanciaSavv/1852).toFixed(1)
+
+console.log(distanciaEntrePuntosFinalUno)
+console.log(distanciaEntrePuntosFinalDos)
+console.log(distanciaEntrePuntosFinalTres)
